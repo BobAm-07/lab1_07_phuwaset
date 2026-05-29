78ed48e78ff17cd07c92296f28d704c597c67a86
BobAm-07 <phuwaset.boonsomtob@e-tech.ac.th>
2026-05-29 16:03:44 +0700

    work1

--- lib/l4.dart
+++ lib/l4.dart
@@ -1,4 +1,46 @@
 void main(){
-  double high,weight,meter,bmi;
+  double high,weight,meter,bmi,result;
+  String status;
+
+  high = 165.0;
+  weight = 80.0;
+   meter = high/100;
+  bmi = weight/(meter*meter);
+
+  if(bmi < 18.5){
+    status = "Thin";
+  }
+  else if(bmi < 25){
+    status = "Normal";
+  }
+  else if(bmi < 30){
+    status = "Plump";
+  }
+  else{
+    status = "Fat";
+  }
   
+  print("Weight: $weight kg, High: $high cm.");
+  print("BMI = ${bmi.toStringAsFixed(2)} Status: $status\n");
+
+  high = 180.0;
+  weight = 65.0;
+  meter = high/100;
+  bmi = weight/(meter*meter);
+
+  if(bmi < 18.5){
+    status = "Thin";
+  }
+  else if(bmi < 25){
+    status = "Normal";
+  }
+  else if(bmi < 30){
+    status = "Plump";
+  }
+  else{
+    status = "Fat";
+  }
+
+  print("Weight: $weight kg, High: $high cm.");
+  print("BMI = ${bmi.toStringAsFixed(2)} Status: $status");
 }
