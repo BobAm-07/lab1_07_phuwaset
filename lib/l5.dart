ce2b3421fdea74e9e4bbbb35e0c80ce13e32e018
BobAm-07 <phuwaset.boonsomtob@e-tech.ac.th>
2026-06-05 13:11:46 +0700

    work1

--- lib/l5.dart
+++ lib/l5.dart
@@ -1,3 +1,23 @@
 void main(){
-  int score;
+  int score = 75;
+  String grade = score >= 80
+      ?'A'
+      :score >= 70
+          ?'B'
+          :score >= 60
+              ?'C'
+              :'F';
+  print('Score: $score  Grade: $grade\n');
+
+  int score2 = 59;
+  String grade2 = score2 >= 80
+      ?'A'
+      :score2 >= 70
+      ?'B'
+      :score2 >= 60
+      ?'C'
+      :'F';
+  print('Score: $score2  Grade: $grade2');
+
+
 }
